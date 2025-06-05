6b5841e569d5eddcd394aaaedd37805299a8e127
lcarlandl <arlandlc@gmail.com>
2025-06-05 08:42:21 -0700

    Test.c parses and maps key values to struct

--- test.c
+++ test.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
 // testing spliting strings by delimiters
+
+int keyValParse(char* input, char delimeter, char *keyBuffer, char *valBuffer) {
+
+	int delim_pos;
+	for (int i = 0; i < strlen(input); i++) {
+		if (input[i] == delimeter) {
+			delim_pos = i;
+		}
+	}
+
+	for (int i = 0; i < delim_pos; i++) {
+		keyBuffer[i] = input[i];
+	}
+
+	int val_index = 0;
+	for (int i = delim_pos + 1; i < strlen(input); i++) {
+		valBuffer[val_index] = input[i];
+		val_index++;
+	}
+
+	return 0;
+}
+
+typedef struct
+{
+	char name[16];
+	char type[16];
+	char status[16];
+} Animal;
+
 
 int main(void) {
 	char list[3][20] = {
@@ -9,29 +40,33 @@ int main(void) {
 		"status=crazy"
 	};
 
-	for (int i = 0; i < 3; i++) {
-		char key[16];
-		char value[24];
-		int value_start;
+	Animal _animal = {
+		.name = "",
+		.type = "",
+		.status = "",
+	};
 
-		for (int c = 0; c < sizeof(list[i]); c++) {
-			char ch = list[i][c];
-			if (ch == 0) {
-				continue;
-			}
-			else if (ch == '=') {
-				value_start = c + 1;
-				break;
-			}
-			key[c] = ch;
+	char key[16];
+	char value[24];
+	for (int i = 0; i < 3; i++) {	
+
+		keyValParse(list[i], '=', key, value);
+
+		if (strcmp(key, "name") == 0) {
+			strcpy(_animal.name, value);
 		}
-		int val_index = 0;
-		for (int c = value_start; c < sizeof(list[i]); c++){
-			value[val_index] = list[i][c];
-			val_index++;
+		else if (strcmp(key, "type") == 0) {
+			strcpy(_animal.type, value);
+		}
+		else if (strcmp(key, "status") == 0) {
+			strcpy(_animal.status, value);
 		}
 
-		printf("Key: %s\n",key);
-		printf("Value: %s\n", value);
+		memset(key, 0, sizeof(key));
+		memset(value, 0, sizeof(value));
 	}
+
+	printf("Name: %s; Type: %s; Status: %s\n", _animal.name, _animal.type, _animal.status);
+
+	return 0;
 }
